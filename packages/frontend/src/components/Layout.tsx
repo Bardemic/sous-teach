57220a68d560ce6b97f9b99ad4c01ed1a4510cfa
Brandon Pieczka <134565367+Bardemic@users.noreply.github.com>
2025-11-16 08:47:17 -0600

    lgtm

--- packages/frontend/src/components/Layout.tsx
+++ packages/frontend/src/components/Layout.tsx
@@ -1,22 +1,17 @@
 import { PropsWithChildren } from 'react';
-import { Header } from './Header';
-import { Footer } from './Footer';
 
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div
       style={{
         minHeight: '100vh',
-        background: '#e2e8f0',
         display: 'flex',
         flexDirection: 'column',
       }}
     >
-      <Header />
       <main style={{ flex: 1, padding: '2rem 1.5rem' }}>
         <div style={{ maxWidth: 960, margin: '0 auto' }}>{children}</div>
       </main>
-      <Footer />
     </div>
   );
 }
